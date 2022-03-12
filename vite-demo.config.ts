f8e6c488597c68b86e1f88928b8f9ccb686366a8
Andrey Parfenov <asleeppiano@outlook.com>
2022-03-12 18:47:48 +0300

    feat: adds github ci workflow
    - Added github ci workflow
    - Added base url to vite demo config

--- vite-demo.config.ts
+++ vite-demo.config.ts
@@ -7,6 +7,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+	base: '/vue-window-component/',
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
